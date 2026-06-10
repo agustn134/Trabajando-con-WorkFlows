5810ca67c5feecba29ee43028fc19e698078682f
agustn134 <agustinparralopez1@gmail.com>
2026-06-10 13:37:28 -0600

    Agrega pipeline CI/CD y configuraciones de Vercel

---
+++ backend/index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('¡Hola desde mi Backend en Vercel!');
+});
+
+// Iniciamos el servidor con app.listen SOLO para entorno local
+if (process.env.NODE_ENV !== 'production') {
+    const PORT = process.env.PORT || 3001;
+    app.listen(PORT, () => console.log(`Servidor local en puerto ${PORT}`));
+}
+
+// Exportamos la app para que Vercel la ejecute como Serverless Function
+module.exports = app;
